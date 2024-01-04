ef8e1d17d1f6f42012463b75d1a9cf28b8011082
Pablo <pablomenor2001@gmail.com>
2024-01-04 14:24:51 +0100

    added standardjs linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: 'https://astro-moon-landing.netlify.app/',
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
-});
+      external: ['svgo']
+    }
+  }
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,31 +1,31 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter Variable', ...defaultTheme.fontFamily.sans]
       },
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)'
       },
       textColor: {
-        default: "var(--color-text)",
-        offset: "var(--color-text-offset)",
+        default: 'var(--color-text)',
+        offset: 'var(--color-text-offset)'
       },
       backgroundColor: {
-        default: "var(--color-background)",
-        offset: "var(--color-background-offset)",
+        default: 'var(--color-background)',
+        offset: 'var(--color-background-offset)'
       },
       borderColor: {
-        default: "var(--color-border)",
-      },
-    },
+        default: 'var(--color-border)'
+      }
+    }
   },
   corePlugins: {
-    fontSize: false,
+    fontSize: false
   },
-  plugins: [require("tailwindcss-fluid-type")],
-};
+  plugins: [require('tailwindcss-fluid-type')]
+}
