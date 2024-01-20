5bcd5b52a4a84dad58b531dad04e61f6861afa5f
Pablo <pablomenor2001@gmail.com>
2024-01-20 23:23:53 +0100

    install & configure vue framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-moon-landing.netlify.app/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), vue()],
   vite: {
     ssr: {
       external: ['svgo']
---
+++ src/components/cv-switcher.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { log } from 'node_modules/astro/dist/core/logger/core'
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const selectedTab = ref(0)
+
+const increment = () => {
+  confetti({
+  particleCount: 700,
+  spread: 500,
+  origin: { y: 0.6 },
+});
+}
+
+// lifecycle hooks
+onMounted(() => {
+  const script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js";
+    script.async = true;
+    document.head.appendChild(script);
+
+})
+</script>
+
+<template>
+  <button @click="increment">Count is: ¡4</button>
+  <h1>defe</h1>
+</template>
